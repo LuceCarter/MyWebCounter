178afb7102e4b57e907e4646728b61090c370955
Luce Carter <luce@lucyscarter.co.uk>
2020-03-16 14:16:12 +0000

    Currently badly coded counter controller - eek

---
+++ MyWebCounter/Controllers/CounterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebCounter.Controllers
+{
+    public class CounterController : Controller
+    {
+        int i = 0;
+        public IActionResult Index()
+        {
+            i++;
+            ViewData["Count"] = i.ToString();
+
+            return View();
+        }        
+    }
+}
